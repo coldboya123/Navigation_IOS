a7246fae8981dbf0b8622a2a5e6972cb7b1a3838
coldboya123 <65642613+coldboya123@users.noreply.github.com>
2021-05-24 19:56:04 +0700

    Initial Commit

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Nguyen Duc Huy on 5/24/21.
+//  Copyright © 2021 Nguyen Duc Huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
